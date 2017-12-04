0dc8e62d4c84e714f4113fa27385ceecd3962de7
Dean Jackson <deanishe@deanishe.net>
2017-12-04 17:38:26 +0100

    Add rules for objective-see.com

---
+++ rules/objective-see.com.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'grabber' => array(
+        '%.*%' => array(
+            'test_url' => 'https://objective-see.com/blog/blog_0x24.html',
+            'body' => array(
+                '//section[@class="blogContent"]',
+            ),
+            'strip' => array(),
+        )
+    )
+);
+?>
+
